e1bf3bf61d4e7d91b246721ee64f13a185a4caaa
HecAllende <54869462+HectorAllende@users.noreply.github.com>
2020-10-12 11:02:50 -0300

    tragos

---
+++ precache-manifest.288c8a86d7f0077fed213d5ff6ab024a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5279f7d1981ac52121a9b3c62357202f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a59e71aa6cbde93c671",
+    "url": "/static/css/main.eebeedd0.chunk.css"
+  },
+  {
+    "revision": "825b13dbfe542f83aca1",
+    "url": "/static/js/2.ead5b3b1.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/static/js/2.ead5b3b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a59e71aa6cbde93c671",
+    "url": "/static/js/main.bffd7d0a.chunk.js"
+  },
+  {
+    "revision": "e7b3ca1efcdbb47cb2f9",
+    "url": "/static/js/runtime-main.ab30fdfe.js"
+  }
+]);
